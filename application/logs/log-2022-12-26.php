0f9f82b4e44746316983adb454697451ae66a8e6
amoriotechnology <madhu@amoriotech.com>
2022-12-26 11:09:06 +0530

    My message

--- application/logs/log-2022-12-26.php
+++ application/logs/log-2022-12-26.php
@@ -1,67 +1,66 @@
-
-ERROR - 2022-12-26 05:37:46 --> $config['composer_autoload'] is set to TRUE but D:\wamp\www\R9-UAT-Testing\application\vendor/autoload.php was not found.
-INFO - 2022-12-26 05:37:46 --> Config Class Initialized
-INFO - 2022-12-26 05:37:46 --> Hooks Class Initialized
-DEBUG - 2022-12-26 05:37:46 --> UTF-8 Support Enabled
-INFO - 2022-12-26 05:37:46 --> Utf8 Class Initialized
-INFO - 2022-12-26 05:37:46 --> URI Class Initialized
-INFO - 2022-12-26 05:37:46 --> Router Class Initialized
-INFO - 2022-12-26 05:37:46 --> Output Class Initialized
-INFO - 2022-12-26 05:37:46 --> Security Class Initialized
-DEBUG - 2022-12-26 05:37:46 --> Global POST, GET and COOKIE data sanitized
-INFO - 2022-12-26 05:37:46 --> CSRF cookie sent
-INFO - 2022-12-26 05:37:46 --> CSRF token verified
-INFO - 2022-12-26 05:37:46 --> Input Class Initialized
-INFO - 2022-12-26 05:37:46 --> Language Class Initialized
-INFO - 2022-12-26 05:37:46 --> Loader Class Initialized
-INFO - 2022-12-26 05:37:46 --> Helper loaded: url_helper
-INFO - 2022-12-26 05:37:46 --> Helper loaded: file_helper
-INFO - 2022-12-26 05:37:46 --> Helper loaded: form_helper
-INFO - 2022-12-26 05:37:46 --> Helper loaded: lang_helper
-INFO - 2022-12-26 05:37:46 --> Database Driver Class Initialized
-INFO - 2022-12-26 05:37:46 --> Session: Class initialized using 'files' driver.
-INFO - 2022-12-26 05:37:46 --> Parser Class Initialized
-INFO - 2022-12-26 05:37:46 --> Language file loaded: language/english/pagination_lang.php
-INFO - 2022-12-26 05:37:46 --> Pagination Class Initialized
-INFO - 2022-12-26 05:37:46 --> Form Validation Class Initialized
-INFO - 2022-12-26 05:37:46 --> Controller Class Initialized
-INFO - 2022-12-26 05:37:46 --> Model "Web_settings" initialized
-DEBUG - 2022-12-26 05:37:46 --> Auth class already loaded. Second attempt ignored.
-DEBUG - 2022-12-26 05:37:46 --> Session class already loaded. Second attempt ignored.
-INFO - 2022-12-26 05:37:46 --> Model "Customers" initialized
-INFO - 2022-12-26 05:37:46 --> Model "Invoices" initialized
-INFO - 2022-12-26 05:37:46 --> Final output sent to browser
-DEBUG - 2022-12-26 05:37:46 --> Total execution time: 0.0122
-ERROR - 2022-12-26 05:37:46 --> $config['composer_autoload'] is set to TRUE but D:\wamp\www\R9-UAT-Testing\application\vendor/autoload.php was not found.
-INFO - 2022-12-26 05:37:46 --> Config Class Initialized
-INFO - 2022-12-26 05:37:46 --> Hooks Class Initialized
-DEBUG - 2022-12-26 05:37:46 --> UTF-8 Support Enabled
-INFO - 2022-12-26 05:37:46 --> Utf8 Class Initialized
-INFO - 2022-12-26 05:37:46 --> URI Class Initialized
-INFO - 2022-12-26 05:37:46 --> Router Class Initialized
-INFO - 2022-12-26 05:37:46 --> Output Class Initialized
-INFO - 2022-12-26 05:37:46 --> Security Class Initialized
-DEBUG - 2022-12-26 05:37:46 --> Global POST, GET and COOKIE data sanitized
-INFO - 2022-12-26 05:37:46 --> CSRF cookie sent
-INFO - 2022-12-26 05:37:46 --> CSRF token verified
-INFO - 2022-12-26 05:37:46 --> Input Class Initialized
-INFO - 2022-12-26 05:37:46 --> Language Class Initialized
-INFO - 2022-12-26 05:37:46 --> Loader Class Initialized
-INFO - 2022-12-26 05:37:46 --> Helper loaded: url_helper
-INFO - 2022-12-26 05:37:46 --> Helper loaded: file_helper
-INFO - 2022-12-26 05:37:46 --> Helper loaded: form_helper
-INFO - 2022-12-26 05:37:46 --> Helper loaded: lang_helper
-INFO - 2022-12-26 05:37:46 --> Database Driver Class Initialized
-INFO - 2022-12-26 05:37:46 --> Session: Class initialized using 'files' driver.
-INFO - 2022-12-26 05:37:46 --> Parser Class Initialized
-INFO - 2022-12-26 05:37:46 --> Language file loaded: language/english/pagination_lang.php
-INFO - 2022-12-26 05:37:46 --> Pagination Class Initialized
-INFO - 2022-12-26 05:37:46 --> Form Validation Class Initialized
-INFO - 2022-12-26 05:37:46 --> Controller Class Initialized
-INFO - 2022-12-26 05:37:46 --> Model "Web_settings" initialized
-DEBUG - 2022-12-26 05:37:46 --> Auth class already loaded. Second attempt ignored.
-DEBUG - 2022-12-26 05:37:46 --> Session class already loaded. Second attempt ignored.
-INFO - 2022-12-26 05:37:46 --> Model "Customers" initialized
-INFO - 2022-12-26 05:37:46 --> Model "Invoices" initialized
-INFO - 2022-12-26 05:37:46 --> Final output sent to browser
-DEBUG - 2022-12-26 05:37:46 --> Total execution time: 0.0086
+ERROR - 2022-12-26 05:38:50 --> $config['composer_autoload'] is set to TRUE but D:\wamp\www\R9-UAT-Testing\application\vendor/autoload.php was not found.
+INFO - 2022-12-26 05:38:50 --> Config Class Initialized
+INFO - 2022-12-26 05:38:50 --> Hooks Class Initialized
+DEBUG - 2022-12-26 05:38:50 --> UTF-8 Support Enabled
+INFO - 2022-12-26 05:38:50 --> Utf8 Class Initialized
+INFO - 2022-12-26 05:38:50 --> URI Class Initialized
+INFO - 2022-12-26 05:38:50 --> Router Class Initialized
+INFO - 2022-12-26 05:38:50 --> Output Class Initialized
+INFO - 2022-12-26 05:38:50 --> Security Class Initialized
+DEBUG - 2022-12-26 05:38:50 --> Global POST, GET and COOKIE data sanitized
+INFO - 2022-12-26 05:38:50 --> CSRF cookie sent
+INFO - 2022-12-26 05:38:50 --> CSRF token verified
+INFO - 2022-12-26 05:38:50 --> Input Class Initialized
+INFO - 2022-12-26 05:38:50 --> Language Class Initialized
+INFO - 2022-12-26 05:38:50 --> Loader Class Initialized
+INFO - 2022-12-26 05:38:50 --> Helper loaded: url_helper
+INFO - 2022-12-26 05:38:50 --> Helper loaded: file_helper
+INFO - 2022-12-26 05:38:50 --> Helper loaded: form_helper
+INFO - 2022-12-26 05:38:50 --> Helper loaded: lang_helper
+INFO - 2022-12-26 05:38:50 --> Database Driver Class Initialized
+INFO - 2022-12-26 05:38:50 --> Session: Class initialized using 'files' driver.
+INFO - 2022-12-26 05:38:50 --> Parser Class Initialized
+INFO - 2022-12-26 05:38:50 --> Language file loaded: language/english/pagination_lang.php
+INFO - 2022-12-26 05:38:50 --> Pagination Class Initialized
+INFO - 2022-12-26 05:38:50 --> Form Validation Class Initialized
+INFO - 2022-12-26 05:38:50 --> Controller Class Initialized
+INFO - 2022-12-26 05:38:50 --> Model "Web_settings" initialized
+DEBUG - 2022-12-26 05:38:50 --> Auth class already loaded. Second attempt ignored.
+DEBUG - 2022-12-26 05:38:50 --> Session class already loaded. Second attempt ignored.
+INFO - 2022-12-26 05:38:50 --> Model "Customers" initialized
+INFO - 2022-12-26 05:38:50 --> Model "Invoices" initialized
+INFO - 2022-12-26 05:38:50 --> Final output sent to browser
+DEBUG - 2022-12-26 05:38:50 --> Total execution time: 0.0194
+ERROR - 2022-12-26 05:38:55 --> $config['composer_autoload'] is set to TRUE but D:\wamp\www\R9-UAT-Testing\application\vendor/autoload.php was not found.
+INFO - 2022-12-26 05:38:55 --> Config Class Initialized
+INFO - 2022-12-26 05:38:55 --> Hooks Class Initialized
+DEBUG - 2022-12-26 05:38:55 --> UTF-8 Support Enabled
+INFO - 2022-12-26 05:38:55 --> Utf8 Class Initialized
+INFO - 2022-12-26 05:38:55 --> URI Class Initialized
+INFO - 2022-12-26 05:38:55 --> Router Class Initialized
+INFO - 2022-12-26 05:38:55 --> Output Class Initialized
+INFO - 2022-12-26 05:38:55 --> Security Class Initialized
+DEBUG - 2022-12-26 05:38:55 --> Global POST, GET and COOKIE data sanitized
+INFO - 2022-12-26 05:38:55 --> CSRF cookie sent
+INFO - 2022-12-26 05:38:55 --> CSRF token verified
+INFO - 2022-12-26 05:38:55 --> Input Class Initialized
+INFO - 2022-12-26 05:38:55 --> Language Class Initialized
+INFO - 2022-12-26 05:38:55 --> Loader Class Initialized
+INFO - 2022-12-26 05:38:55 --> Helper loaded: url_helper
+INFO - 2022-12-26 05:38:55 --> Helper loaded: file_helper
+INFO - 2022-12-26 05:38:55 --> Helper loaded: form_helper
+INFO - 2022-12-26 05:38:55 --> Helper loaded: lang_helper
+INFO - 2022-12-26 05:38:55 --> Database Driver Class Initialized
+INFO - 2022-12-26 05:38:55 --> Session: Class initialized using 'files' driver.
+INFO - 2022-12-26 05:38:55 --> Parser Class Initialized
+INFO - 2022-12-26 05:38:55 --> Language file loaded: language/english/pagination_lang.php
+INFO - 2022-12-26 05:38:55 --> Pagination Class Initialized
+INFO - 2022-12-26 05:38:55 --> Form Validation Class Initialized
+INFO - 2022-12-26 05:38:55 --> Controller Class Initialized
+INFO - 2022-12-26 05:38:55 --> Model "Web_settings" initialized
+DEBUG - 2022-12-26 05:38:55 --> Auth class already loaded. Second attempt ignored.
+DEBUG - 2022-12-26 05:38:55 --> Session class already loaded. Second attempt ignored.
+INFO - 2022-12-26 05:38:55 --> Model "Customers" initialized
+INFO - 2022-12-26 05:38:55 --> Model "Invoices" initialized
+INFO - 2022-12-26 05:38:55 --> Final output sent to browser
+DEBUG - 2022-12-26 05:38:55 --> Total execution time: 0.0188
